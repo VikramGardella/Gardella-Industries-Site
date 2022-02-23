352229d6d5598110e0d0043ec5879034302eaaf0
Vik Gardella <60987280+VikramGardella@users.noreply.github.com>
2022-02-23 01:56:39 -0500

    clean upload
    publishing directly to azure service from visual studio on Mac resulted in html that had been hit by a virtual tornado apparently; going to do manually from azure's command line like before.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5&units=imperial&lang=en",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
+		"x-rapidapi-key": "19733aad2amsh509b66398b420c3p1ca0c4jsn90e3652a863e"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
